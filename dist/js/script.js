d14184a2488982fbc6ba2dda9e9e7dce3ad19328
a.marleku <a.marleku@gmail.com>
2020-03-18 23:39:45 +0100

    Push Files

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+var request = new XMLHttpRequest()
+request.open('GET', 'https://coronavirus-19-api.herokuapp.com/countries/Albania', true)
+request.onload = function() {
+  var data = JSON.parse(this.response)
+  document.getElementById("cases").innerHTML = data.cases;
+  document.getElementById("recovered").innerHTML = data.recovered;
+  document.getElementById("deaths").innerHTML = data.deaths;
+}
+request.send()
