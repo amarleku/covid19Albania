0b54908af98d76d89089c489c5b1003b890bc511
a.marleku <a.marleku@gmail.com>
2020-03-20 18:52:19 +0100

    Global numbers

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,16 @@ request.onload = function() {
   document.getElementById("critical").innerHTML = data.critical;
 }
 request.send()
+
+
+
+var request = new XMLHttpRequest()
+request.open('GET', 'https://coronavirus-19-api.herokuapp.com/all', true)
+request.onload = function() {
+  var data = JSON.parse(this.response)
+  document.getElementById("allCases").innerHTML = data.cases;
+  document.getElementById("allRecovered").innerHTML = data.recovered;
+  document.getElementById("allDeaths").innerHTML = data.deaths;
+
+}
+request.send()
