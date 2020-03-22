ec7e673bd68d5a2b0e95d768d4836d46593d542f
a.marleku <a.marleku@gmail.com>
2020-03-22 13:43:35 +0100

    Changes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,15 @@
-var request;(request=new XMLHttpRequest).open("GET","https://coronavirus-19-api.herokuapp.com/countries/Albania",!0),request.onload=function(){var e=JSON.parse(this.response);document.getElementById("cases").innerHTML=e.cases,document.getElementById("recovered").innerHTML=e.recovered,document.getElementById("deaths").innerHTML=e.deaths,document.getElementById("todayCases").innerHTML=e.todayCases,document.getElementById("critical").innerHTML=e.critical},request.send(),(request=new XMLHttpRequest).open("GET","https://coronavirus-19-api.herokuapp.com/all",!0),request.onload=function(){var e=JSON.parse(this.response);document.getElementById("allCases").innerHTML=e.cases,document.getElementById("allRecovered").innerHTML=e.recovered,document.getElementById("allDeaths").innerHTML=e.deaths},request.send();
+var request;
+(request = new XMLHttpRequest).open("GET", "https://coronavirus-19-api.herokuapp.com/countries/Albania", !0), 
+request.onload = function () {
+  var e = JSON.parse(this.response);
+  document.getElementById("cases").innerHTML = e.cases,
+   document.getElementById("recovered").innerHTML = e.recovered, 
+   document.getElementById("deaths").innerHTML = e.deaths,
+    document.getElementById("todayCases").innerHTML = e.todayCases, 
+    document.getElementById("critical").innerHTML = e.critical,
+    document.getElementById("active").innerHTML = e.active
+}, 
+request.send(), (request = new XMLHttpRequest).open("GET", "https://coronavirus-19-api.herokuapp.com/all", !0), request.onload = function () {
+  var e = JSON.parse(this.response);
+  document.getElementById("allCases").innerHTML = e.cases, document.getElementById("allRecovered").innerHTML = e.recovered, document.getElementById("allDeaths").innerHTML = e.deaths
+}, request.send();
